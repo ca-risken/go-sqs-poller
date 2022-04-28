fe50d3fc12e44c0826b49c17b09062d897d1faeb
gassara-kys <ogasawara_kiyoshi@cybernagent>
2022-04-28 17:56:30 +0900

    use: risken logging

--- worker/v5/worker.go
+++ worker/v5/worker.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/ca-risken/common/pkg/logging"
 )
 
 // HandlerFunc is used to define the Handler that is run on for each message
@@ -55,7 +56,7 @@ type QueueDeleteReceiverAPI interface {
 // Worker struct
 type Worker struct {
 	Config    *Config
-	Log       LoggerIFace
+	Log       logging.Logger
 	SqsClient QueueDeleteReceiverAPI
 }
 
@@ -74,7 +75,7 @@ func New(client QueueAPI, config *Config) *Worker {
 
 	return &Worker{
 		Config:    config,
-		Log:       &logger{},
+		Log:       logging.NewLogger(),
 		SqsClient: client,
 	}
 }
@@ -87,7 +88,7 @@ func (worker *Worker) Start(ctx context.Context, h Handler) {
 			log.Println("worker: Stopping polling because a context kill signal was sent")
 			return
 		default:
-			worker.Log.Debug("worker: Start Polling")
+			worker.Log.Debug(ctx, "worker: Start Polling")
 
 			params := &sqs.ReceiveMessageInput{
 				QueueUrl:            aws.String(worker.Config.QueueURL), // Required
@@ -104,16 +105,16 @@ func (worker *Worker) Start(ctx context.Context, h Handler) {
 				continue
 			}
 			if len(resp.Messages) > 0 {
-				worker.run(h, resp.Messages)
+				worker.run(ctx, h, resp.Messages)
 			}
 		}
 	}
 }
 
 // poll launches goroutine per received message and wait for all message to be processed
-func (worker *Worker) run(h Handler, messages []*sqs.Message) {
+func (worker *Worker) run(ctx context.Context, h Handler, messages []*sqs.Message) {
 	numMessages := len(messages)
-	worker.Log.Info(fmt.Sprintf("worker: Received %d messages", numMessages))
+	worker.Log.Info(ctx, fmt.Sprintf("worker: Received %d messages", numMessages))
 
 	var wg sync.WaitGroup
 	wg.Add(numMessages)
@@ -121,8 +122,8 @@ func (worker *Worker) run(h Handler, messages []*sqs.Message) {
 		go func(m *sqs.Message) {
 			// launch goroutine
 			defer wg.Done()
-			if err := worker.handleMessage(m, h); err != nil {
-				worker.Log.Error(err.Error())
+			if err := worker.handleMessage(ctx, m, h); err != nil {
+				worker.Log.Error(ctx, err.Error())
 			}
 		}(messages[i])
 	}
@@ -130,11 +131,11 @@ func (worker *Worker) run(h Handler, messages []*sqs.Message) {
 	wg.Wait()
 }
 
-func (worker *Worker) handleMessage(m *sqs.Message, h Handler) error {
+func (worker *Worker) handleMessage(ctx context.Context, m *sqs.Message, h Handler) error {
 	var err error
 	err = h.HandleMessage(m)
 	if _, ok := err.(InvalidEventError); ok {
-		worker.Log.Error(err.Error())
+		worker.Log.Error(ctx, err.Error())
 	} else if err != nil {
 		return err
 	}
@@ -147,7 +148,7 @@ func (worker *Worker) handleMessage(m *sqs.Message, h Handler) error {
 	if err != nil {
 		return err
 	}
-	worker.Log.Debug(fmt.Sprintf("worker: deleted message from queue: %s", aws.StringValue(m.ReceiptHandle)))
+	worker.Log.Debug(ctx, fmt.Sprintf("worker: deleted message from queue: %s", aws.StringValue(m.ReceiptHandle)))
 
 	return nil
 }
